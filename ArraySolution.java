d9d7b4bf851d84e380e6717e3dadb92d3a3163e3
khanvilkar-omkar <omu13jan@gmail.com>
2026-06-20 21:11:51 -0400

    commit once again

--- ArraySolution.java
+++ ArraySolution.java
@@ -109,29 +109,31 @@ public class ArraySolution {
      * @return
      */
     public static int[] topKFrequent(int[] nums, int k) {
-        // Map<Integer, Integer> mapping = new HashMap<>();
-        // for (int n : nums) {
-        //     if (mapping.containsKey(n)) {
-        //         mapping.put(n, mapping.getOrDefault(mapping.get(n), 0) + 1);
-        //     } else {
-        //         mapping.put(n, 1);
-        //     }
-        // }
-        // List<Integer>[] buckets = new List[nums.length + 1];
-        // mapping.forEach((num, freq) -> {
-        //     if (buckets[freq] == null) {
-        //         buckets[freq] = new ArrayList<>();
-        //     }
-        //     buckets[freq].add(num);
-        // });
-        // List<Integer> result = new ArrayList<>();
-        // for (int i = buckets.length - 1; i > 0 && result.size() < k; i--) {
-        //     if (buckets[i] != null) {
-        //         result.addAll(buckets[i]);
-        //     }
-        // }
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
 
-        // return result.stream().mapToInt(i -> i).toArray();
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
 
     }
 
@@ -151,7 +153,7 @@ public class ArraySolution {
         // String[] strs = { "act", "pots", "tops", "cat", "stop", "hat" };
         // System.out.println(groupAnagrams(strs));
 
-        int[] nums = { 1, 2, 2, 3, 3, 3, 4, 5, 5, 5 };
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
         System.out.println(Arrays.toString(topKFrequent(nums, 2)));
 
     }
